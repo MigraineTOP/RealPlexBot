2077417876e2b23642c0035a129d315ebb284db3
MigraineTOP <35163815+MigraineTOP@users.noreply.github.com>
2018-03-15 23:23:24 -0500

    Create balance.js

---
+++ commands/balance.js
@@ -0,0 +1,11 @@
+const db = require('quick.db');
+const Discord = require('discord.js')
+const client = new Discord.Client();
+
+exports.run = async (client, message, args, tools) => {
+    let user = message.mentions.users.first() || message.author;
+
+    let balance = await db.fetch(`userBalance_${user.id}`);
+    if (balance === null) balance = 0;
+    message.channel.send(`${user.username} - Balance $${balance}`);
+}

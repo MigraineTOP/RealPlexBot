8c52f8b9dee1a77355f58b78e917cc4b3f37a882
MigraineTOP <35163815+MigraineTOP@users.noreply.github.com>
2018-03-15 23:24:07 -0500

    Create messages.js

---
+++ commands/random/messages.js
@@ -0,0 +1,11 @@
+const db = require("quick.db")
+
+exports.run = (bot, message, args, func) => {
+
+    db.fetchObject(message.author.id + message.guild.id).then(i => { // This is the object of messages sent
+        db.fetchObject(`userLevel_${message.author.id + message.guild.id}`).then(o => { // This is the object of their level
+            message.channel.send('Messages sent: `' + (i.value + 1) + '`\nLevel: `' + o.value + '`'); 
+        })
+    })
+
+}
